c4d00f073bf0d665ef4d5f10010dfd4765b2215e
jing_zheng <jing_zheng@comcast.com>
2022-03-10 17:00:28 -0500

    created search function

--- Tree/Node.py
+++ Tree/Node.py
@@ -30,20 +30,52 @@ class Node:
         if self.right:
             self.right.print_node()
 
+    def search(self, key):
+        if self is None:
+            print("root is empty")
+            return False
+        if key < self.val:
+            if self.left:
+                return self.left.search(key)
+            else:
+                return False
+        elif key > self.val:
+            if self.right:
+                return self.right.search(key)
+            else:
+                return False
+        else:
+            return True
+
 
 class BST:
     def __init__(self):
         self.root = None
 
+    def create_bst(self, lst):
+        if len(lst) == 0:
+            print("List is empty")
+            return
+        if self.root is None:
+            self.root = Node(lst[0])
+
+        if len(lst) > 1:
+            for val in lst[1:]:
+                self.root.insert(val)
+
+    def search_bst(self, key):
+        return self.root.search(key)
+
 
 if __name__ == '__main__':
-    root = Node(10)
-    root.insert(8)
-    root.insert(15)
-    root.insert(7)
-    root.insert(9)
-    root.insert(13)
-    root.insert(4)
-    root.print_node()
+    lst = [10, 8, 15, 7, 9, 13, 4]
+    bst = BST()
+    bst.create_bst(lst)
+    bst.root.print_node()
+    print(bst.search_bst(4))
+    print(bst.search_bst(9))
+    print(bst.search_bst(14))
+
+
 
 
